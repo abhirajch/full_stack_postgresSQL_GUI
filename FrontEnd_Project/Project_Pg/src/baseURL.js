95081d568d9db2b941f6b114a73116b0ccdc9090
abhirajch <124265586+abhirajch@users.noreply.github.com>
2025-07-28 16:52:54 +0530

    Update baseURL.js

--- FrontEnd_Project/Project_Pg/src/baseURL.js
+++ FrontEnd_Project/Project_Pg/src/baseURL.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://full-stack-postgressql-gui-3.onrender.com/api/auth', // your backend base URL
+  baseURL: 'https://full-stack-postgressql-gui-6.onrender.com/api/auth', // your backend base URL
 });
 
 // Add a request interceptor to inject token
