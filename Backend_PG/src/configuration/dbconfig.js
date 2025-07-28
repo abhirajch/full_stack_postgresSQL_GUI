b6a8bd7d04309571e3a97cc9bf8b2f6c6eaefccd
abhirajch <abhiraj93500@gmail.com>
2025-07-28 16:48:38 +0530

    'changes'

--- Backend_PG/src/configuration/dbconfig.js
+++ Backend_PG/src/configuration/dbconfig.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: "postgres://postgres:Abhiraj%40123@dpg-cooldb12345.pg.render.com:5432/project_pg",
   ssl: false,
 });
 
