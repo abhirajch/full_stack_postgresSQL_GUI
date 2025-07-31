5ebf1c884aec6dcd2ef8a77a4d0216ee54120aa3
abhirajch <abhiraj93500@gmail.com>
2025-07-31 11:57:39 +0530

    'changes'

--- FrontEnd_Project/Project_Pg/src/components/header/AppHeaderDropdown.js
+++ FrontEnd_Project/Project_Pg/src/components/header/AppHeaderDropdown.js
@@ -34,8 +34,8 @@ const AppHeaderDropdown = () => {
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">Account</CDropdownHeader>
-        <CDropdownItem href="#">
-          <CIcon icon={cilUser} className="me-2" onClick={handleProfile}/>
+        <CDropdownItem  onClick={handleProfile}>
+          <CIcon icon={cilUser} className="me-2"/>
           Profile
         </CDropdownItem>
         <CDropdownItem href="#">
