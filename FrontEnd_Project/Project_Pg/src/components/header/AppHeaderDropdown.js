657e1a261fbf0ba34c371465c13bb5d33e6e7761
abhirajch <abhiraj93500@gmail.com>
2025-07-31 15:34:41 +0530

    'changes'

--- FrontEnd_Project/Project_Pg/src/components/header/AppHeaderDropdown.js
+++ FrontEnd_Project/Project_Pg/src/components/header/AppHeaderDropdown.js
@@ -19,6 +19,7 @@ import avatar8 from './../../assets/images/avatars/8.jpg'
 
 const AppHeaderDropdown = () => {
   const navigate = useNavigate()
+  const [name, setName] = useState('User')
   const handleLogout = () => {
     localStorage.removeItem('authToken')
     navigate('/login') 
@@ -27,14 +28,19 @@ const AppHeaderDropdown = () => {
   const handleProfile = ()=>{
     navigate('/profile')
   }
-  const name = localStorage.getItem("full_name")
+    useEffect(() => {
+    const storedName = localStorage.getItem("full_name")
+    if (storedName) {
+      setName(storedName)
+    }
+  }, [])
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
         <CAvatar src={avatar8} size="md" />
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
-        <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">{name != null ? name : "User"}</CDropdownHeader>
+        <CDropdownHeader className="bg-body-secondary fw-semibold mb-2">{name}</CDropdownHeader>
         <CDropdownItem  onClick={handleProfile}>
           <CIcon icon={cilUser} className="me-2"/>
           Profile
